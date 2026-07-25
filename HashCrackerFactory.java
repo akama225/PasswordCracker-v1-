8eef8d300bc925f036e5a090cf4fba1fd457d21d
Nafissatou Faye <nafissatou5.faye@ucad.edu.sn>
2026-07-25 22:45:26 +0000

    Update HashCrackerFactory.java

--- HashCrackerFactory.java
+++ HashCrackerFactory.java
@@ -1,9 +1,3 @@
-/**
- * Fabrique responsable de la création des objets HashCracker.
- * Centralise l'instanciation des stratégies concrètes : le programme
- * principal ne connaît jamais DictionaryHashCracker ni BruteForceHashCracker
- * directement, seulement l'interface HashCracker.
- */
 public class HashCrackerFactory {
 
     private static final String DEFAULT_DICTIONARY_PATH = "dictionary.txt";
